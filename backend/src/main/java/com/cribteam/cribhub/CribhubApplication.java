2cbe284fb56b2a467430e40407ab91e8c1da9f71
Marius Hatland <marius.hatland@student.uib.no>
2024-03-04 10:27:53 +0100

    initial commit

---
+++ backend/src/main/java/com/cribteam/cribhub/CribhubApplication.java
@@ -0,0 +1,13 @@
+package com.cribteam.cribhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CribhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CribhubApplication.class, args);
+	}
+
+}
