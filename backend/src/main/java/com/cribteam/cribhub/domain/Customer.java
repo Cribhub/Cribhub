58c982ca82242542f2dc37c6087222e227df72a0
mikkelgunhildsbu <mikkelgunhildsbu@gmail.com>
2024-03-11 14:10:11 +0100

    update build.gradle to fix getter/setter

--- backend/src/main/java/com/cribteam/cribhub/domain/Customer.java
+++ backend/src/main/java/com/cribteam/cribhub/domain/Customer.java
@@ -6,10 +6,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
+@Entity
 @Setter
 public class Customer {
+    // Getters
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
@@ -30,40 +31,6 @@ public class Customer {
     public Customer(String userName, String email, String password) {
         this.userName = userName;
         this.email = email;
-        this.password = password;
-    }
-
-    // Getters
-    public Long getUserId() {
-        return userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    // Setters
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
