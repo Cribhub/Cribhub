46f0b52d38280bea919b7ba6e0637bed9545cc10
mikkelgunhildsbu <mikkelgunhildsbu@gmail.com>
2024-03-12 19:32:33 +0100

    La til login funksjoner ogsånt, men man må være medlem av en crib

--- frontend/src/Pages/CreateAccount.js
+++ frontend/src/Pages/CreateAccount.js
@@ -4,8 +4,15 @@ import Input from '../Components/TextInput'
 import Button from '../Components/Button/Button'
 import {NavLink} from "react-router-dom";
 import {useState} from "react";
+import {useNavigate} from "react-router-dom"
+import axios from "axios";
+
+
 
 function CreateAccount() {
+
+    const navigate = useNavigate();
+
 
     const [emailValue, setEmailValue] = useState('');
     const [usernameValue, setUsernameValue] = useState('');
@@ -28,8 +35,35 @@ function CreateAccount() {
         setConfrimPasswordValue(newValue)
     }
 
+    const validateEmail = (email) => {
+        return email.match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    }
+
+
     function createAccountButton(){
-        //TODO
+        if (emailValue === '' || passwordValue === '' || usernameValue === '' || confrimPasswordValue === ''){
+            alert("Fill out all fields")
+        } else if(!validateEmail(emailValue)){
+            alert("Enter a valid email")
+        } else if(passwordValue !== confrimPasswordValue){
+            alert("password does no match")
+        } else {
+            let user = {
+                "userName": usernameValue,
+                "email": emailValue,
+                "password": passwordValue}
+
+            axios.post("http://localhost:8080/customer", user).then(response => {
+                navigate("/" )
+                console.log(response)
+            }).catch(error => {
+                console.error("Error creating user:", error);
+            });
+
+
+        }
     }
 
     return(
--- frontend/src/Pages/MainMenu.js
+++ frontend/src/Pages/MainMenu.js
@@ -1,15 +1,44 @@
 import './Login.css'
-
 import Button from '../Components/Button/Button'
 import {Calendar} from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
 import "./MainMenu.css"
+import Header from '../Components/Header/Header'
+import Cookies from 'js-cookie';
+
 
 function MainMenu() {
+
+    let token = Cookies.get("Token")
+
+    const payload = parseJwt(token);
+    let userName = payload.customerName;
+    let cribName = payload.customerCribName;
+
+    console.log(userName)
+
+
+    function parseJwt(token) {
+        try {
+            const base64Url = token.split('.')[1]; // Get the payload part of the token
+            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+
+            return JSON.parse(jsonPayload);
+        } catch (e) {
+            return null;
+        }
+    }
 
 
     return(
         <div className={"mainMenuContainer"}>
+            <Header
+                userName={userName}
+                crib={cribName}
+            />
             <div className={"firstrow"}>
                 <div id={"taskList"}>
                     <p>TASK LIST</p>
